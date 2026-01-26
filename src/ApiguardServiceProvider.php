a5157069ee2aeb0b96de2f534a3ab6a210acb5cb
Amer Chaudhary <amermchaudry@gmail.com>
2026-01-26 20:25:16 +0500

    Fixing typos and testing

--- src/ApiguardServiceProvider.php
+++ src/ApiguardServiceProvider.php
@@ -2,6 +2,44 @@
 
 namespace Nodesol\Apiguard;
 
-class ApiguardServiceProvider {
-    
+// use App\Models\User;
+// use App\Services\AuthApiService;
+// use GraphQL\Error\UserError;
+
+use Exception;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\ServiceProvider;
+
+class ApiguardServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        \Log::info("GOT HERE");
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        \Log::info("GOT HERE");
+        // Auth::viaRequest('api-token', function () {
+            // throw new Exception("TEST");
+            // $authApiService = new AuthApiService;
+            // $response = $authApiService->getUser();
+
+            // if (! $response || ! isset($response['data']['CurrentUser'])) {
+            //     throw new UserError('Authentication failed: Invalid user data received from Auth API.');
+            // }
+
+            // $data = $response['data']['CurrentUser'];
+            // $user = new User(...$data);
+            // $user->fill($data);
+
+            // return $user;
+        // });
+    }
 }
